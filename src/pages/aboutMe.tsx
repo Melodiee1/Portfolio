46b35e9b85eed6e861ad63e349133e9b7b1f7461
Melodiee1 <ajeshkharel5@gmail.com>
2026-03-22 13:02:00 +0545

    Styling and improving the main page and header

--- src/pages/aboutMe.tsx
+++ src/pages/aboutMe.tsx
@@ -18,11 +18,11 @@ function AboutMe() {
                         +
                         <div className="typeScript">Typescript</div>
                     </div>
-                    <div className="dev">Developer</div>
+                    <div className="dev"> Developer</div>
                 </div> <br /> <br />
 
                 <div className="text">
-                    Introducing myself as a <u>14 student from Nepal</u>, Koshi (south Asia), am a student of <a href="https://www.facebook.com/pashupati.school.jhiljhile/" target='_blank'>Pashupati Academy</a> currently in grade 8. Beyond that, I'm a self taught React + Typesript Developer who likes to bring rough ideas into clean web experiences. You can navigate to my projects from the navigation bar above.
+                    Introducing myself as a <u>14 year old student from Nepal</u>, Koshi (south Asia), am a student of <a href="https://www.facebook.com/pashupati.school.jhiljhile/" target='_blank'>Pashupati Academy</a> currently in grade 8. Beyond that, I'm a self taught React + Typesript Developer who likes to bring rough ideas into clean web experiences. You can navigate to my projects from the navigation bar above.
                 </div> <br /> <br /> <br />
 
                 <div className="get_two_know_me">
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,7 +2,9 @@
 function Contact() {
 
     return (
-        <h1>COTNACT</h1>
+        <>
+                
+        </>
     )
 }
 export default Contact;
